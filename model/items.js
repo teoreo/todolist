9d347b6919661063b189c405335c77aad697acc3
Emma Teo <emmateo@Emmas-MBP-3.mediegruppen.se>
2020-02-11 13:46:43 +0100

    Gjort knappar till pagination

--- model/items.js
+++ model/items.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const schemaList = new mongoose.Schema(
     {
         text: {type: String, required: true,minlength:5},
-        done: {type: String, required:true, minlength:2}
+        done: {type: String, required:true, minlength:3}
     }
 ) 
 const Item = mongoose.model("Item", schemaList)
--- router/listRouter.js
+++ router/listRouter.js
@@ -21,13 +21,13 @@ await addItem.save((error,success)=>{
   });
   //renderar till ejs
   router.get("/todo", async (req, res) => {
-    const todos = 3;
+    const todoItem = 3;
     const page = req.query.page;
       //const sorted = req.query.sort;
     const items = await Item
     .find().sort({text:1})
     .skip()
-    .limit((page-1) * todos)
+    .limit((page-1) * todoItem)
   res.render("todo",{items});
   })
 
